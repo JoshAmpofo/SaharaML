7284bd40a4ad3307173050c816a1fd88f3a01f42
JoshAmpofo <joshampofo92@gmail.com>
2026-01-02 14:21:39 +0000

    complete bilstm model class

--- TriageML/models/model_bilstm.py
+++ TriageML/models/model_bilstm.py
@@ -30,7 +30,7 @@ class BiLSTMClassifier(nn.Module):
         
         # padding_idx ensures PAD tokens get zero embeddings and not learn semantics
         self.embedding = nn.Embedding(
-            num_embedings=vocab_size,
+            num_embeddings=vocab_size,
             embedding_dim=embedding_dim,
             padding_idx=pad_id,
         )
@@ -46,6 +46,39 @@ class BiLSTMClassifier(nn.Module):
         
         self.dropout = nn.Dropout(dropout)
         # BiLSTM outputs 2*hidden_dim because forward and backward states are concatenated
-        self.classifier = nn.Layer(2 * hidden_dim, num_classes)
+        self.classifier = nn.Linear(2 * hidden_dim, num_classes)
+        
+    
+    def forward(self, input_ids: torch.Tensor, lengths: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass:
+        - input_ids: Tensor of shape (batch_size, seq_length) with token IDs
+        - lengths: Tensor of shape (batch_size,) with actual lengths of each sequence (before padding)
+        
+        Returns:
+        - logits: Tensor of shape (batch_size, num_classes) with class scores
+        
+        - packing sequences prevents the LSTM from "seeing" padded timesteps, improving both efficiency and learning quality
+        """
+        x = self.embedding(input_ids)  # (batch_size, seq_length, embedding_dim)
+        
+        packed = nn.utils.rnn.pack_padded_sequence(
+            x,
+            lengths.cpu(),
+            batch_first=True,
+            enforce_sorted=False, # allows random batch order from DataLoader
+        )
+        
+        packed_output, (h_n, _) = self.lstm(packed)
+        # h_n shape: (num_layers * 2, batch_size, hidden_dim)
+        # we want the last layer's forward and backward idden states
+        forward_last = h_n[-2, :, :]  # (batch_size, hidden_dim)
+        backward_last = h_n[-1, :, :]  # (batch_size, hidden_dim)
+        
+        features = torch.cat([forward_last, backward_last], dim=1)  # (batch_size, 2*hidden_dim)
+        features = self.dropout(features)
+        logits = self.classifier(features)  # (batch_size, num_classes)
+        
+        return logits
         
         
